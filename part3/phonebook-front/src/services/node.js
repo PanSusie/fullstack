d8aaf03ea619fe2a804455eb5628efbd1568772c
parapp <Susie*950828>
2021-02-09 17:33:30 +0200

    part3

---
+++ part3/phonebook-front/src/services/node.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const url = '/api/persons'
+
+const getPersons = () =>{
+  const request = axios.get(url)
+  return request.then(response => response.data)
+}
+
+const addPerson = (newPerson) =>{
+  const request = axios.post(url, newPerson)
+  return request.then(response => response.data)
+}
+
+const deletePerson = (id) =>{
+  const request = axios.delete( `${url}/${id}`)
+  return request.then(response => response.data)
+}
+
+const updatePerson =(id, target) =>{
+  const request = axios.put(`${url}/${id}`, target)
+  return request.then(response => response.data)
+}
+
+export default { getPersons, addPerson, deletePerson, updatePerson }
